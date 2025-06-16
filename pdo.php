1037f2a57b675c21f5907d3f6b6d9625d23516c2
divine462 <divinesafari23@gmail.com>
2025-06-16 19:52:41 +0200

    Initialisation du projet Archéo-IT

---
+++ pdo.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$dbname = 'archeo_it';
+$username = 'root';
+$password = ''; 
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "Connexion réussie";
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+?>
